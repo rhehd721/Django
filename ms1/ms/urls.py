54a81e322f029f0afd5bbffe73c5b407b70aa411
parkbyeongho <rhehd721@likelion.org>
2020-07-28 22:10:53 +0900

    Django로 나를 소개해볼꼐 #1

--- ms1/ms/urls.py
+++ ms1/ms/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 import msapp.views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +12,4 @@ urlpatterns = [
     path('introduce/', msapp.views.introduce, name='introduce'),
     path('test1/', msapp.views.test1, name='test1'),
     path('test2/', msapp.views.test2, name='test2')
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- ms1/msapp/admin.py
+++ ms1/msapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Designer
+
+admin.site.register(Designer)
 
 # Register your models here.
