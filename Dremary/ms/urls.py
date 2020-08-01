8157fd6bd26a0fc449c491db7b0a979ffe3675aa
알파카 <rhehd721@naver.com>
2020-08-01 15:42:37 +0900

    망한거2

---
+++ Dremary/ms/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+# import msapp.views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('msapp.urls')),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\알파카\desktop\django\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
